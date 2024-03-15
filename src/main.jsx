d334cd5ba3cef3ddea21c49a8bb9cc3f5a00e423
Florcdb <florcordoba51@gmail.com>
2024-03-15 15:47:06 +0100

    completo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,64 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+
+import { useState } from 'react';
+function App(){
+  const [name, newName] = useState('Sofía')
+  const setNameData = ()=> {
+    newName('Data')
+  }
+
+  const setNameReyes = () => {
+    newName('Reyes')
+  }
+
+  const setNameYolanda = () => {
+    newName('Yolanda')
+  }
+  return (
+    <div>
+      <h2>Teacher Name: {name} </h2>
+        <ul>
+        <li onClick={setNameData}>Data</li>
+        <li onClick={setNameReyes}>Reyes</li>
+        <li onClick={setNameYolanda}>Yolanda</li>
+        </ul> 
+
+        <button></button>
+    </div>
+  )
+}
+/*
+
+import { useState } from 'react';
+function App(){
+  const [name, newName] = useState('Sofía')
+  const changeName = () => {
+    newName('')
+  }
+
+
+
+
+  return (
+    <div>
+      <h2>Teacher Name: {name} </h2>
+        <form onSubmit={handleSubmit}>
+        <input 
+         type="text"
+         value={newName} 
+         onChange={changeName}
+         placeholder="Add a name"/>
+        <button className='buttonform' type='submit' placeholder> Add</button>
+        </form> 
+    </div>
+    )
+}
+
+*/
+export default App 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
